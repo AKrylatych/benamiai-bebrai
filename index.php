647f51d43e5cdc8ff298e8e1bb5bceb4263fc488
Hellerlight <hellerlight@gmail.com>
2023-05-21 21:31:45 +0300

    Dev branching

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ try {
     $conn = new PDO("mysql:host=$servername:3306;dbname=$dbname", $username, $password);
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Connected successfully. \n Hooray!";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
