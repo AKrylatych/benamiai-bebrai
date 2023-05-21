83fc77cb4a4f095521144ee8e05549ade387f5bf
Hellerlight <hellerlight@gmail.com>
2023-05-21 21:48:16 +0300

    testing

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
-include('dbcreds.php');
+//include('dbcreds.php');
+//$servername = "mysql:3306"; // Or the IP address of the Docker container
+$servername = $_ENV['Server']; // Or the IP address of the Docker container
+$username = $_ENV['Username'];
+$password = $_ENV['Password'];
+$database = "userdb"; // Replace with the actual database name
+
 echo "Testing environment variables.<br>";
 echo "Servername: $servername<br>";
-echo "DBName: $dbname<br>";
+echo "DBName: $database<br>";
 echo "Username: $username<br>";
 echo "Password: $password<br>";
+echo "<br><br>---";
+print_r($_ENV);
+echo "<br><br>---";
 
 try {
-    $conn = new PDO("mysql:host=$servername:3306;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername:3306;dbname=$database", $username, $password);
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully. \n Hooray!";
