a6fae9fea7db6f5befba928cb97c142ca1b18d83
Hellerlight <hellerlight@gmail.com>
2023-05-21 21:20:03 +0300

    OpenShift test

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-$servername = "localhost:3306"; // Or the IP address of the Docker container
-$username = "root";
-$password = "root";
-$dbname = "vartotoju_db"; // Replace with the actual database name
+include('dbcreds.php');
 
+echo "Servername: $servername\n";
+echo "DBName: $dbname\n";
+echo "Username: $username\n";
+echo "Password: $password\n";
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$servername:3306;dbname=$dbname", $username, $password);
     // Set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-echo "My first PHP script!";
 ?>
