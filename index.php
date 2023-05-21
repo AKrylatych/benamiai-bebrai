bc12f9a6dddcb606e587fca0aac346d5c7ddc09d
Hellerlight <hellerlight@gmail.com>
2023-05-21 22:12:47 +0300

    env edits, $_ENV Doesnt work

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 //include('dbcreds.php');
 //$servername = "mysql:3306"; // Or the IP address of the Docker container
-$servername = $_ENV['Server']; // Or the IP address of the Docker container
-$username = $_ENV['Username'];
-$password = $_ENV['Password'];
+$servername = getenv('Server'); // Or the IP address of the Docker container
+$username = getenv('Username');
+$password = getenv('Password');
 $database = "userdb"; // Replace with the actual database name
 
 echo "Testing environment variables.<br>";
@@ -11,14 +11,6 @@ echo "Servername: $servername<br>";
 echo "DBName: $database<br>";
 echo "Username: $username<br>";
 echo "Password: $password<br>";
-echo "<br><br>---";
-print_r($_ENV);
-echo "<br><br>---";
-echo "<br>Testing environment variables Again.<br>";
-echo "Servername:", getenv('Server'), "<br>";
-echo "DBName: $database<br>";
-echo "Username:", getenv('Username'), "<br>";
-echo "Password:", getenv('Password'), "<br>";
 try {
     $conn = new PDO("mysql:host=$servername:3306;dbname=$database", $username, $password);
     // Set the PDO error mode to exception
