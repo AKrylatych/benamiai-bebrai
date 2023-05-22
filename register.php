3168780aa8989ba59b1b94151ad3b1a15191e0e3
Hellerlight <hellerlight@gmail.com>
2023-05-22 10:40:30 +0300

    Added placeholder user addition, testing database response.

--- register.php
+++ register.php
@@ -1,16 +1,16 @@
 <?php
 include("usercontrol.php");
-
-if (isset($_GET['vardas']) && isset($_GET['slaptazodis'])) {
-    $vartotojo_vardas = $_GET['vardas'];
-    $vartotojo_slaptazodis = $_GET['slaptazodis'];
+echo "i am registering.";
+//if (isset($_GET['vardas']) && isset($_POST['slaptazodis'])) {
+    $vartotojo_vardas = $_POST['vardas'];
+    $vartotojo_slaptazodis = $_POST['slaptazodis'];
     echo "vartotojo vardas; $vartotojo_slaptazodis";
     echo "<br> Vartotojo ivestas slaptazodis: $vartotojo_slaptazodis<br>";
     $userctl = new usercontrol();
     $userctl->addTempUser($vartotojo_vardas, $vartotojo_slaptazodis);
     echo "user added successfully.<br>";
-
-} else {
-    echo "No params, failed login.";
-}
+//
+//} else {
+//    echo "No params, failed login.";
+//}
 
