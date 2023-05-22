0785c2f997bec226d09de12af5e44a7df858cfc5
Hellerlight <hellerlight@gmail.com>
2023-05-22 20:25:41 +0300

    existing user check

--- usercontrol.php
+++ usercontrol.php
@@ -5,10 +5,14 @@ class usercontrol {
     public function addUser($name, $insecure_passwd, $email)  {
         $hashctl = new hashcontrol();
         $dbctl = new dbcontrol();
-//        $this->findUserbyName($name);
-        $hashed_passwd = $hashctl->get_hashed_password($insecure_passwd);
-        $dbctl->insertNormalUser($name, $hashed_passwd, $email);
-
+        if ($this->findUserbyName($name) != null) {
+            $hashed_passwd = $hashctl->get_hashed_password($insecure_passwd);
+            $dbctl->insertNormalUser($name, $hashed_passwd, $email);
+        } else {
+            echo "Toks vartotojas jau egzistuoja. Prasome naudoti kita varda.";
+            sleep (4);
+            header("Location: index.html");
+        }
     }
     public function loginUser($name, $insecure_passwd) {
 //        TODO: Logins
