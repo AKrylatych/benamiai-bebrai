defcadda4f342b62e659910d4018e3ac994ec8b2
Hellerlight <hellerlight@gmail.com>
2023-05-22 10:21:12 +0300

    Added placeholder user addition, testing database response.

---
+++ usercontrol.php
@@ -0,0 +1,37 @@
+<?php
+include('hashcontrol.php');
+class usercontrol extends dbcontrol {
+    public function adduser($name, $surname, $email, $insecure_passwd)  {
+        echo "name: $name";
+        echo "Inputted password: $insecure_passwd<br>";
+//        echo "surname: $surname<br>";
+//        echo "email: $email<br>";
+        $hashctl = new hashcontrol();
+        $hashed_passwd = $hashctl->get_hashed_password($insecure_passwd);
+        $this->insertUser($name, $hashed_passwd);
+
+    }
+
+    public function addTempUser($name, $insecure_passwd) {
+        echo "name: $name";
+        echo "Inputted password: $insecure_passwd<br>";
+//        echo "surname: $surname<br>";
+//        echo "email: $email<br>";
+        $hashctl = new hashcontrol();
+        $hashed_passwd = $hashctl->get_hashed_password($insecure_passwd);
+        $this->insertUser($name, $hashed_passwd);
+
+    }
+
+    protected function checkuser($name, $insecure_password) {
+
+    }
+
+    protected function findUserbyName($name) {
+        return $this->findValueinColumn($name, "Vardas", "vartotojai");
+    }
+
+    protected function getUserPasswordbyID($UID) {
+        $this->getValuebyID($UID, "Slaptazodis", "vartotojai");
+    }
+}
