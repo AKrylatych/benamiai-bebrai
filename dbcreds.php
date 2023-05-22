dc63062fbcc94408944a277d2b5ab6c9a9274d73
Hellerlight <hellerlight@gmail.com>
2023-05-22 17:57:44 +0300

    Fixed connection failed to database. change is accompanied by database table altering to include autoincrement.

--- dbcreds.php
+++ dbcreds.php
@@ -2,5 +2,5 @@
 $servername = getenv('Server');
 $username = getenv('Username');
 $password = getenv('Password');
-$user_database = "benamiai_bebrai";
+$database = "benamiai_bebrai";
 // TODO: Animal database
