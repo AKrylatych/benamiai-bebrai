833fd8c95103b4cf05f831fb44288b983ea9129d
Hellerlight <hellerlight@gmail.com>
2023-05-23 19:21:13 +0300

    usertable testing

--- guicontrol.php
+++ guicontrol.php
@@ -6,6 +6,7 @@ class guicontrol
         $dbctl = new dbcontrol();
         $table = $dbctl->selectUserTable();
         $columns = $table->fetch_fields();
+        echo "<form>";
         echo "<table>";
         echo "<thead>";
         echo "<tr>";
@@ -20,18 +21,23 @@ class guicontrol
             echo "<tr>";
             // Generuoja eilutes
             $colnum = 0;
+            $rowid = 0;
             foreach ($row as $value) {
                 switch ($colnum) {
+                    case 0:
+                        $rowid = $value;
+                        break;
                     case 3:
                         echo "<td> * * * </td>";
                         break;
                     case 4:
                         echo "<td>" . $value . "</td>";
-                        echo "<td>Trinti</td>";
-                        echo "<td>Redaguoti</td>";
+                        echo "<td><input type='button' name='delete_row' value='$rowid'>Trinti</input></td>";
+                        echo "<td><input type='button' name='edit_row' value='$rowid'>Redaguoti</input></td>";
+                        echo "<td>::Rowid - $rowid::</td>";
                         break;
                     default:
-                        echo "<td>" . $colnum . "|" . $value . "</td>";
+                        echo "<td>" . $value . "</td>";
                         break;
                 }
                 $colnum++;
@@ -40,5 +46,7 @@ class guicontrol
         }
         echo "</tbody>";
         echo "</table>";
+        echo "</form>";
+
     }
 }
